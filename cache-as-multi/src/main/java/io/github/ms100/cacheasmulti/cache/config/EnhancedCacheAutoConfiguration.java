8dbe2fba52ff3e35b7327625c8a3e47611426eb6
ms <ms1.0@qq.com>
2023-02-01 15:51:59 +0800

    去掉不生效的判断

--- cache-as-multi/src/main/java/io/github/ms100/cacheasmulti/cache/config/EnhancedCacheAutoConfiguration.java
+++ cache-as-multi/src/main/java/io/github/ms100/cacheasmulti/cache/config/EnhancedCacheAutoConfiguration.java
@@ -11,15 +11,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.AbstractCachingConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.annotation.ProxyCachingConfiguration;
-import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportAware;
 import org.springframework.context.annotation.Role;
-import org.springframework.core.annotation.AnnotationAttributes;
-import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.Collection;
 
@@ -28,25 +23,7 @@ import java.util.Collection;
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnBean(ProxyCachingConfiguration.class)
-public class EnhancedCacheAutoConfiguration extends AbstractCachingConfiguration implements ImportAware {
-
-    @Override
-    public void setImportMetadata(AnnotationMetadata importMetadata) {
-        AnnotationAttributes attributes = AnnotationAttributes.fromMap(
-                importMetadata.getAnnotationAttributes(EnableCaching.class.getName()));
-        if (attributes == null) {
-            throw new IllegalArgumentException(String.format(
-                    "@%s is not present on importing class '%s' as expected",
-                    EnableCaching.class.getSimpleName(), importMetadata.getClassName()));
-        }
-
-        AdviceMode adviceMode = attributes.getEnum("mode");
-
-        if (adviceMode == AdviceMode.ASPECTJ) {
-            throw new IllegalArgumentException(
-                    "Mode AspectJ is not supported in cache-as-multi");
-        }
-    }
+public class EnhancedCacheAutoConfiguration extends AbstractCachingConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
